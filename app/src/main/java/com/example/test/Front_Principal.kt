406d85e60488a490c4d8f8d28e6f5817c484889f
ManuelPenaDev <manuel.pena.quezada@ciisa.cl>
2022-10-02 16:06:49 -0300

    Estructura

---
+++ app/src/main/java/com/example/test/Front_Principal.kt
@@ -0,0 +1,11 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Front_Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front_principal)
+    }
+}
---
+++ app/src/main/java/com/example/test/Menu_Mapa.kt
@@ -0,0 +1,11 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_Mapa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_mapa)
+    }
+}
