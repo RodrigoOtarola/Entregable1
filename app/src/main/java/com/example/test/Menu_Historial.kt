d302fcd24be019eead44ec53fe60056c5a34f85d
ManuelPenaDev <manuel.pena.quezada@ciisa.cl>
2022-10-29 01:28:39 -0300

    menu_Historial

--- app/src/main/java/com/example/test/Menu_Historial.kt
+++ app/src/main/java/com/example/test/Menu_Historial.kt
@@ -1,11 +1,44 @@
 package com.example.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Menu_Historial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_historial)
+
+        // declaro variables
+        val btn_servicio1 = findViewById<ImageButton>(R.id.btn_servicio1)
+        val btn_servicio2 = findViewById<ImageButton>(R.id.btn_servicio2)
+        val btn_servicio3= findViewById<ImageButton>(R.id.btn_servicio3)
+        val btn_return = findViewById<ImageButton>(R.id.btn_return)
+
+        //BOTON SERVICIO 1 POR DEFINIR
+        btn_servicio1.setOnClickListener {
+            val intent = Intent(this@Menu_Historial,Menu_Realizar_Reserva::class.java)
+            startActivity(intent)
+        }
+
+        //BOTON SERVICIO 2 POR DEFINIR
+        btn_servicio2.setOnClickListener {
+            val intent = Intent(this@Menu_Historial,Servicio::class.java)
+            startActivity(intent)
+        }
+
+        //BOTON SERVICIO 3 POR DEFINIR
+        btn_servicio3.setOnClickListener {
+            val intent = Intent(this@Menu_Historial,Menu_Historial::class.java)
+            startActivity(intent)
+        }
+
+        //BOTON VOLVER A MENU RESERVA
+        btn_return.setOnClickListener {
+            val intent = Intent(this@Menu_Historial,Menu_Reservas::class.java)
+            startActivity(intent)
+        }
     }
 }
+
