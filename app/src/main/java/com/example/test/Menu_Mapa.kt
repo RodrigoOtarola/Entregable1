a513a3110fbf3aa2430d32b191a57bed9a0fc8c7
ManuelPenaDev <manuel.pena.quezada@ciisa.cl>
2022-10-28 23:57:02 -0300

    menu_mapa

--- app/src/main/java/com/example/test/Menu_Mapa.kt
+++ app/src/main/java/com/example/test/Menu_Mapa.kt
@@ -1,11 +1,24 @@
 package com.example.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Menu_Mapa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_mapa)
+
+
+        //declaro variable
+        val btn_MenuMapaML = findViewById<ImageButton>(R.id.btn_MenuMapa)
+
+        //Boton volver
+        btn_MenuMapaML.setOnClickListener {
+            val intent = Intent(this@Menu_Mapa, Menu_Locales::class.java)
+            startActivity(intent)
+        }
+
     }
 }
